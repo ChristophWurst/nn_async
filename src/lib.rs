ab8e504e2f183aa87d8af01f2a5a4ee601f44b68
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-10-12 23:02:28 +0200

    Go fully async with the help of futures and a cpu pool
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
+extern crate futures;
+extern crate futures_cpupool;
 extern crate rand;
 
 mod nn;
 
+use futures::Future;
+use futures_cpupool::CpuPool;
 use rand::Rng;
+use std::sync::Arc;
 use std::vec::Vec;
 
 pub fn find_nn() {
+    let pool = CpuPool::new_num_cpus();
     let mut rand = rand::thread_rng();
 
     let points: Vec<nn::Point> = (0..50000)
@@ -17,8 +23,20 @@ pub fn find_nn() {
             }
         })
         .collect();
-    let indices: Vec<usize> = (0..points.len())
-        .map(|idx| nn::find_closest(&points, idx))
-        .collect();
-    indices.iter().take(10).for_each(|idx| println!("{}", idx));
+
+    let mut futures: Vec<futures_cpupool::CpuFuture<usize, ()>> = Vec::with_capacity(points.len());
+    let points = Arc::new(points);
+    for idx in 0..points.len() {
+        let pz = points.clone();
+        let x = idx.clone();
+        let fut = pool.spawn_fn(move || futures::future::ok(nn::find_closest(&pz, x)));
+        futures.push(fut);
+    }
+
+    futures::future::join_all(futures)
+        .map(|results| {
+            results.iter().take(10).for_each(|idx| println!("{}", idx));
+        })
+        .wait()
+        .expect("not to fail");
 }
